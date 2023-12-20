7600572a52783ce06c1b0bcf0725a5470dca2d7e
nikhilsinghpy <nikhilsingh1139@gmail.com>
2023-12-20 22:54:48 +0530

    add second slider

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -13,3 +13,25 @@ function displaynavbar(){
   navlinks.classList.toggle("active")
   // var nav = document.getElementsByClassName()
 }    
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
