2f130acd11ad5c63b9683b245922ff5121bcff82
nikhilsinghpy <nikhilsingh1139@gmail.com>
2023-12-24 12:04:37 +0530

    add slider

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -8,7 +8,7 @@ function displaynavbar(){
 }    
 
 
-var swiper = new Swiper(".slide-content", {
+var swiper = new Swiper("#slide-content-first", {
   slidesPerView: 4,
   spaceBetween: 25,
   loop: true,
@@ -21,8 +21,37 @@ var swiper = new Swiper(".slide-content", {
     dynamicBullets: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: "#next-btn",
+    prevEl: "#prev-btn",
+  },
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
+
+var secondswiper = new Swiper("#swiper-content-second", {
+  slidesPerView: 4,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: "#next-btn-second",
+    prevEl: "#prev-btn-second",
   },
 
   breakpoints:{
